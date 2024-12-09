7bb0a60af689ca56df8294add83440810e1daa1e
GirishHakki <109576032+GirishHakki@users.noreply.github.com>
2024-12-09 22:12:38 +0530

    Add files via upload

---
+++ day14.py
@@ -0,0 +1,13 @@
+# perfect numbers
+# perfect numbers means sum of factors is equal to the number
+
+num = int(input("Enter a number : "))
+s = 0
+for a in range(1, num):
+    if num % a == 0:
+        s += a
+
+if s == num:
+    print(num, "is a perfect number")
+else:
+    print(num, "is not a perfect number")
